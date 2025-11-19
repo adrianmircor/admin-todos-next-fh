c4bddd7821dcaef3347f1bc8a07a44e80131863d
Adrian <adrianmircor@gmail.com>
2025-11-19 00:24:13 -0500

    revert & correccion

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -17,7 +17,7 @@ export default function ProfilePage() {
       <hr />
       <div className="flex flex-col">
         <span>{session?.user?.name ?? 'No name'}</span>
-        <span>{session?.user?.image ?? 'No iamge'}</span>
+        <span>{session?.user?.image ?? 'No image'}</span>
         <span>{session?.user?.email ?? 'No email'}</span>
         <span>{session?.user?.id ?? 'No UUID'}</span>
         <span>{session?.user?.roles?.join(',') ?? ['no-roles']}</span>
